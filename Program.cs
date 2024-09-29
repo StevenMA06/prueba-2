4f87354605c2c7f73e698366cbd54b23ba94fa01
Fabri <fabricioronaldogil@gmail.com>
2024-09-29 11:59:29 -0500

    nombre

--- Program.cs
+++ Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("pepe 02");
 Console.WriteLine("Goooooooooo the forest");
 Console.WriteLine("prueva");
+Console.WriteLine("Goooooooooo the forest");
